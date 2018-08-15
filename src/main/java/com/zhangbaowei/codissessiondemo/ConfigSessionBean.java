5ce0fdb846b2ec32dd921fb9a5ac8b05f603ef7b
zhangbaowei <zbw911@gmail.com>
2018-08-15 14:55:37 +0800

    初步可运行

--- src/main/java/com/zhangbaowei/codissessiondemo/ConfigSessionBean.java
+++ src/main/java/com/zhangbaowei/codissessiondemo/ConfigSessionBean.java
@@ -1,18 +1,23 @@
 package com.zhangbaowei.codissessiondemo;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.session.RedisSessionProperties;
+import org.springframework.boot.autoconfigure.session.SessionProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.RedisOperationsSessionRepositoryImpl;
-import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
+import org.springframework.util.StringUtils;
+
+import java.time.Duration;
 
 /**
  * Create by  zhangbaowei on 2018/8/15 10:40.
@@ -36,10 +41,10 @@ public class ConfigSessionBean {
         return redisTemplate;
     }
 
-    @Bean
-    public ConfigureRedisAction configureRedisAction() {
-        return ConfigureRedisAction.NO_OP;
-    }
+//    @Bean
+//    public ConfigureRedisAction configureRedisAction() {
+//        return ConfigureRedisAction.NO_OP;
+//    }
 
     @Configuration
     public class SpringBootRedisHttpSessionConfiguration
@@ -47,38 +52,78 @@ public class ConfigSessionBean {
 
         @Autowired
         RedisOperations<Object, Object> sessionRedisOperations;
-
-//        @Autowired
-//        public void customize(SessionProperties sessionProperties,
-//                              RedisSessionProperties redisSessionProperties) {
-//            Duration timeout = sessionProperties.getTimeout();
-//            if (timeout != null) {
-//                setMaxInactiveIntervalInSeconds(100);
-//            }
-//            setRedisNamespace(redisSessionProperties.getNamespace());
-//            setRedisFlushMode(redisSessionProperties.getFlushMode());
-//            setCleanupCron(redisSessionProperties.getCleanupCron());
-//        }
+        @Autowired
+        SessionProperties sessionProperties;
+        @Autowired
+        RedisSessionProperties redisSessionProperties;
+        @Autowired
+        RedisConnectionFactory redisConnectionFactory;
 
         @Bean
         @Override
         public RedisOperationsSessionRepository sessionRepository() {
-            //RedisTemplate<Object, Object> redisTemplate = sessionRedisOperations;
-//            RedisOperationsSessionRepository sessionRepository = new RedisOperationsSessionRepository(
-//                    sessionRedisOperations);
 
             RedisOperationsSessionRepositoryImpl sessionRepository = new RedisOperationsSessionRepositoryImpl(sessionRedisOperations);
-            //  sessionRepository.setApplicationEventPublisher(this.applicationEventPublisher);
-//            if (this.defaultRedisSerializer != null) {
-//                sessionRepository.setDefaultSerializer(this.defaultRedisSerializer);
-//            }
-            sessionRepository
-                    .setDefaultMaxInactiveInterval(100);
-//            if (StringUtils.hasText(this.redisNamespace)) {
-//                sessionRepository.setRedisKeyNamespace(this.redisNamespace);
-//            }
-//            sessionRepository.setRedisFlushMode(this.redisFlushMode);
+
+            Duration timeout = sessionProperties.getTimeout();
+          /*
+           # 下面的代码是在父类里初始化时候用的，暂时看注掉也没关系
+          if (timeout != null) {
+                setMaxInactiveIntervalInSeconds((int) timeout.getSeconds());
+            }
+            setRedisNamespace(redisSessionProperties.getNamespace());
+            setRedisFlushMode(redisSessionProperties.getFlushMode());
+            setCleanupCron(redisSessionProperties.getCleanupCron());*/
+
+
+            if (timeout != null) {
+                sessionRepository
+                        .setDefaultMaxInactiveInterval((int) timeout.getSeconds());
+            }
+
+            if (StringUtils.hasText(redisSessionProperties.getNamespace())) {
+                sessionRepository.setRedisKeyNamespace(redisSessionProperties.getNamespace());
+            }
+            sessionRepository.setRedisFlushMode(redisSessionProperties.getFlushMode());
+
             return sessionRepository;
+        }
+
+
+        /**
+         * 跳过Redis “Config”  命令
+         *
+         * @return
+         */
+//        @Override
+        @Bean
+        public InitializingBean enableRedisKeyspaceNotificationsInitializer() {
+            return null;
+        }
+
+        /**
+         * 跳过发布-订阅命令
+         *
+         * @return
+         */
+        @Bean
+        @Override
+        public RedisMessageListenerContainer redisMessageListenerContainer() {
+            RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+            container.setConnectionFactory(this.redisConnectionFactory);
+//            if (this.redisTaskExecutor != null) {
+//                container.setTaskExecutor(this.redisTaskExecutor);
+//            }
+//            if (this.redisSubscriptionExecutor != null) {
+//                container.setSubscriptionExecutor(this.redisSubscriptionExecutor);
+//            }
+//            container.addMessageListener(sessionRepository(),
+//                    Arrays.asList(new PatternTopic("__keyevent@*:del"),
+//                            new PatternTopic("__keyevent@*:expired")));
+//            container.addMessageListener(sessionRepository(),
+//                    Collections.singletonList(new PatternTopic(
+//                            sessionRepository().getSessionCreatedChannelPrefix() + "*")));
+            return container;
         }
 
     }
